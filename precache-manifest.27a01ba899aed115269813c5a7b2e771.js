2acf5e028e0a7c73553c0f421c9165a874619e13
Aarsh Patel <arm@v.local>
2020-12-21 00:34:46 -0500

    init

---
+++ precache-manifest.27a01ba899aed115269813c5a7b2e771.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea78fe184160085a83a88fe3fdf1916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "694f0689736bc8fee4a8",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d7e58bb145257516db63",
+    "url": "/static/css/main.55c85ee6.chunk.css"
+  },
+  {
+    "revision": "694f0689736bc8fee4a8",
+    "url": "/static/js/2.2660207d.chunk.js"
+  },
+  {
+    "revision": "d7e58bb145257516db63",
+    "url": "/static/js/main.8af96d03.chunk.js"
+  },
+  {
+    "revision": "61f1058c873dd87e78ef",
+    "url": "/static/js/runtime~main.1d98b63e.js"
+  },
+  {
+    "revision": "53ac3e1ee1b1da802c336476c9863674",
+    "url": "/static/media/arduino.53ac3e1e.jpg"
+  },
+  {
+    "revision": "d3ac313448dde4d9e67327c23d1d6cfd",
+    "url": "/static/media/e.d3ac3134.png"
+  },
+  {
+    "revision": "0b38290792d92e61254ee268f600b1d6",
+    "url": "/static/media/fl.0b382907.png"
+  },
+  {
+    "revision": "906146a27314475eb5eba1f880df950c",
+    "url": "/static/media/img1.906146a2.png"
+  },
+  {
+    "revision": "41e6286ec495b2143749eb7c39362d4f",
+    "url": "/static/media/ptc.41e6286e.png"
+  },
+  {
+    "revision": "b64de7cbaa1a8ef66ef88c9bc962f9e4",
+    "url": "/static/media/resale.b64de7cb.png"
+  },
+  {
+    "revision": "ff334ee51c5651726147bf297a0506e4",
+    "url": "/static/media/sc.ff334ee5.png"
+  }
+]);
